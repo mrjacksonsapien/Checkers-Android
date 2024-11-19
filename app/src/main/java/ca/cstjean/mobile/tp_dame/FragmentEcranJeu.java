979701ab6a163e01f2cca114a965b4106b54b250
Martin Soltan <mrtnsoltan@gmail.com>
2024-11-18 19:23:25 -0500

    Works but cannot display id :(

--- app/src/main/java/ca/cstjean/mobile/tp_dame/FragmentEcranJeu.java
+++ app/src/main/java/ca/cstjean/mobile/tp_dame/FragmentEcranJeu.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
 import androidx.fragment.app.Fragment;
 
 /**
@@ -12,7 +14,6 @@ import androidx.fragment.app.Fragment;
  * create an instance of this fragment.
  */
 public class FragmentEcranJeu extends Fragment {
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +22,33 @@ public class FragmentEcranJeu extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ecran_jeu, container, false);
+        View view = inflater.inflate(R.layout.fragment_ecran_jeu, container, false);
+        Jeu jeu = new Jeu(new Damier(), false);
+        jeu.getDamier().initialiser();
+        GridLayout interfaceDamier = view.findViewById(R.id.damier);
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i % 2 == 0) {
+                    inflater.inflate(R.layout.case_blanche, interfaceDamier);
+
+                    Button caseNoire = (Button) inflater.inflate(R.layout.case_noire, interfaceDamier, false);
+                    int id = (i * 5) + j;
+                    caseNoire.setId(id);
+                    caseNoire.setText(String.valueOf(id));
+                    interfaceDamier.addView(caseNoire);
+                } else {
+                    Button caseNoire = (Button) inflater.inflate(R.layout.case_noire, interfaceDamier, false);
+                    int id = (i * 5) + j;
+                    caseNoire.setId(id);
+                    caseNoire.setText(String.valueOf(id));
+                    interfaceDamier.addView(caseNoire);
+
+                    inflater.inflate(R.layout.case_blanche, interfaceDamier);
+                }
+            }
+        }
+
+        return view;
     }
 }
